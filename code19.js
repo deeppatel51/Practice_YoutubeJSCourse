45ceb863a5012b12c806cfe4fa2c7d1d7086c93b
Deep Patel <deeppatel51@outlook.com>
2022-08-12 16:06:24 +0530

    Update code19.js

--- code19.js
+++ code19.js
@@ -1,1 +1,20 @@
 // loops
+let num = [2,3,4,5,6];
+
+for(let i =0; i< num.length; i++)
+{
+    console.log(num[i]);
+}
+
+num.forEach((element) => {
+    console.log(element+"\n")
+})
+
+
+for (let i of num){
+    console.log(i+"\n");
+}
+
+for (let i in num){
+    console.log(i+"\n");
+}
